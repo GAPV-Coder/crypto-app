8c052705189a8daa8e1e8041d8b7ba03a6421910
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2023-03-30 18:19:39 -0500

    feat: create RTK-query for crypto news

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+	REACT_APP_NEWS_RAPIDAPI_HOST,
+	REACT_APP_RAPIDAPI_KEY,
+    REACT_APP_NEWS_API_URL
+} from '../config';
+
+const cryptoNewsHeaders = {
+	'x-bingapis-sdk': 'true',
+	'x-rapidapi-key': REACT_APP_RAPIDAPI_KEY,
+	'x-rapidapi-host': REACT_APP_NEWS_RAPIDAPI_HOST
+};
+
+const createRequest = url => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+	reducerPath: 'cryptoNewsApi',
+	baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_NEWS_API_URL }),
+	endpoints: builder => ({
+		getCryptoNews: builder.query({
+			query: ({ newsCategory, count }) =>
+				createRequest(
+					`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+				)
+		})
+	})
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
