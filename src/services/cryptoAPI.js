ed171c57b0b74f38bd680f81d835859e2e173d50
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2023-03-30 21:11:28 -0500

    perf: Update RTK-queries

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -19,9 +19,24 @@ export const cryptoApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl }),
 	endpoints: builder => ({
 		getCryptos: builder.query({
-			query: (count) => createRequest(`/coins?limit=${count}`)
+			query: count => createRequest(`/coins?limit=${count}`)
+		}),
+		getCryptoDetails: builder.query({
+			query: coinId => createRequest(`/coin/${coinId}`)
+		}),
+		getCryptoHistory: builder.query({
+			query: ({ coinId, timeperiod }) =>
+				createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`)
+		}),
+		getExchanges: builder.query({
+			query: () => createRequest('/exchanges')
 		})
 	})
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const {
+	useGetCryptosQuery,
+	useGetCryptoDetailsQuery,
+	useGetExchangesQuery,
+	useGetCryptoHistoryQuery
+} = cryptoApi;
